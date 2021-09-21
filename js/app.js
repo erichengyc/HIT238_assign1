2545a46650f76ffd3aad935b51ee2f2d35ddb985
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-09-21 23:19:23 +0930

    service worker activation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js')
-        .then(reg=>console.log('sw registered', reg))
-        .catch(err=>console.log('sw register err', err))
-    }
+        .then(reg => console.log('sw registered', reg))
+        .catch(err => console.log('sw register err', err))
+}
---
+++ pages/sw.js
@@ -0,0 +1,9 @@
+// install process
+self.addEventListener('install', e => {
+    console.log('sw is installed')
+})
+
+// activate process
+self.addEventListener('activate', e => {
+    console.log('sw is activated')
+})
