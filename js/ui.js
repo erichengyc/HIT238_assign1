7ecd765580b8b353fd54b3d50e3a5712617a3274
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-09-21 21:58:13 +0930

    initial create
    create page with materialize css and js

---
+++ js/ui.js
@@ -0,0 +1,5 @@
+// FUNCTION FOR sidenav navbar 
+document.addEventListener('DOMContentLoaded', function() {  
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
