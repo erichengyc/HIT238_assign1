204b44c99396ea4049e7f47e5d8466c56e3eebea
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-09-21 23:48:43 +0930

    cache the app asset

--- sw.js
+++ sw.js
@@ -1,9 +1,27 @@
+const staticCache = 'static-cache';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/materialize.min.js',
+    '/css/materialize.min.css',
+    'https://fonts.googleapis.com/icon?family=Material+Icons'
+
+]
+
 // install process
 self.addEventListener('install', e => {
-    console.log('sw is installed')
+    e.wa
+    caches.open(staticCache).then(cache => {
+        cache.addAll(assets)
+    })
 })
 
 // activate process
 self.addEventListener('activate', e => {
     console.log('sw is activated')
+})
+
+self.addEventListener('fetch', e=>{
+    console.log('sw fetch event', e)
 })
